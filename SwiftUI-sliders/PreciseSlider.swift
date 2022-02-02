e16666209d902873a2da622838f6a71f367f4852
Šimon Strýček <xstryc06@vutbr.cz>
2022-02-02 21:43:22 +0100

    Added value and scale selection mechanisms. Implemented axis visualization.

--- SwiftUI-sliders/PreciseSlider.swift
+++ SwiftUI-sliders/PreciseSlider.swift
@@ -9,17 +9,51 @@ import SwiftUI
 
 struct PreciseSlider: View {
     @State var value: Double = 0.0
+    @State var prevValue: Double = 0.0
+    
     @State var scale: Double = 1.0
+    @State var prevScale: Double = 1.0
     
-    private let numberOfUnits: Int = 41
-    private let defaultStep: CGFloat = 10.0
-    
-    private var designUnit: CGFloat {
-        return CGFloat(defaultStep) * scale
+    private var truncScale: Double {
+        return scale / scaleBase
     }
     
+    private var scaleBase: Double {
+        let exponent = floor(log(scale) / log(5))
+        return pow(5, exponent)
+    }
+    
+    // Počet jednotek osy
+    private let numberOfUnits: Int = 41
+    // Výchozí vzdálenost mezi jednotkami
+    private let defaultStep: CGFloat = 10.0
+    
+    // Reálná hodnota zobrazené jednotky
+    private var unit: CGFloat {
+        return Double(defaultStep) / scaleBase
+    }
+    
+    // Grafická vzdálenost jedné jednotky
+    private var designUnit: CGFloat {
+        return CGFloat(defaultStep) * truncScale
+    }
+    
+    // Index středu osy
     private var middleIndex: Int {
         return (numberOfUnits / 2) + 1
+    }
+    
+    // Posuv osy v rámci jedné jednotky
+    private var offset: CGFloat {
+        var truncValue = value.truncatingRemainder(dividingBy: unit)
+        
+        // Oprava nepřesnosti funkce truncatingReminder
+        if (unit - truncValue) < (unit / 1000) {
+            truncValue = 0
+        }
+
+        // Transformace do báze vizualizace
+        return (truncValue / unit) * designUnit
     }
     
     var body: some View {
@@ -41,14 +75,42 @@ struct PreciseSlider: View {
                 .offset(getUnitOffset(ofIndex: index))
             }
         }
+        // Výběr hodnoty
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    value = prevValue + (gesture.translation.width / scale)
+                }
+                .onEnded { _ in
+                    prevValue = value
+                }
+        )
+        // Výběr měřítka
+        .gesture(
+            MagnificationGesture()
+                .onChanged { gesture in
+                    let newScale = prevScale * gesture.magnitude
+                    
+                    // Ošetření minimální hodnoty
+                    scale = newScale > 1 ? newScale : 1.0
+                }
+                .onEnded { _ in
+                    prevScale = scale
+                }
+        )
     }
     
     private func getUnitHeight(ofIndex index: Int) -> CGFloat {
-        return 15.0
+        if getRelativeIndex(ofIndex: index) % 5 != 0 {
+            let height = (truncScale - 1) / 3 * 20
+            return height < 20.0 ? height : 20.0
+        }
+        
+        return 20.0
     }
     
     private func getUnitOffset(ofIndex index: Int) -> CGSize {
-        let offset = (CGFloat(index) * designUnit) - (CGFloat(middleIndex) * designUnit)
+        let offset = (CGFloat(index) * designUnit) - (CGFloat(middleIndex) * designUnit) + offset
         
         return .init(width: offset, height: .zero)
     }
@@ -63,6 +125,13 @@ struct PreciseSlider: View {
     
     private func getUnitLabel(ofIndex index: Int) -> String {
         return ""
+    }
+    
+    private func getRelativeIndex(ofIndex index: Int) -> Int {
+        return index
+            - Int((value / unit)
+                    .truncatingRemainder(dividingBy: 5))
+            - Int(middleIndex % 5)
     }
 }
 
